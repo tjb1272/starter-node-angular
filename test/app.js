775da1f47af6c131e720c7ca961a71efdfe118f1
tjb1272 <tjb1272@gmail.com>
2018-04-11 19:12:22 -0700

    update test

--- test/app.js
+++ test/app.js
@@ -1,5 +1,13 @@
 var assert = require('assert');
 
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+    });
+});
+
 describe('Math', function() {
     it('should test if 3*3 = 9', function() {
         assert.equal(9, 3 * 3);
